94a7b87ae45cd50d8751f7e87fe2578d0ac430e7
EmilianoGarin <emiliano.garin333@gmail.com>
2023-08-30 12:22:28 -0700

    fix

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,5 +8,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
+    """task 3"""
     coro = wait_random(max_delay)
     return asyncio.create_task(coro)
