8d5f7a6ee270fd2dfa2ae8c1431ee0fc359a22b0
EmilianoGarin <emiliano.garin333@gmail.com>
2023-08-30 13:10:01 -0700

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -6,7 +6,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """task 1"""
-    ret = []
-    async for x in async_generator():
-        ret.append(x)
-    return ret
+    return [x async for x in async_generator()]
